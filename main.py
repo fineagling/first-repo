e3a015d6c548744a30abcf1f42d04404f84be8bc
Finian Eagling <Finjude123@outlook.com>
2024-08-11 01:31:57 +0100

    first working menu

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-import pygame
+import pygame, sys
+from engine import Button
 pygame.init()
-num = 7
-def add(num):
-    num2 = num + 1
-    return num2
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Maze Generator and Solver")
+menu_backGround = pygame.image.load("assets/background.png")
+red = pygame.Color(255,0,0)
+black = pygame.Color(0,0,0)
+green = pygame.Color(0,255,0)
+white = pygame.Color(255,255,255)
+def get_font(size):
+    return pygame.font.Font("assets/font.ttf", size)
 
-print(add(num))
+run = True
+def one_player():
+    screen.blit(menu_backGround, (0,0))
+    
+def two_player():
+    screen.blit(menu_backGround, (0,0))
 
-#this is a test
+
+def main_menu():
+    while run == True:
+        screen.blit(menu_backGround, (0,0))
+        menu_mouse_pos = pygame.mouse.get_pos()
+        heading_main_menu_text = get_font(100).render("MAIN MENU", True, "#b68f40")
+        menu_rect = heading_main_menu_text.get_rect(center=(640,100))
+
+        ONE_PLAYER_BUTTON = Button(pos=(640,250), button_font=get_font(40), base_colour= white, hovering_colour="#d7fcd4", input_text="1 PLAYER", image=None, x_start=475, y_start=200, x_end=780, y_end=300)
+        TWO_PLAYER_BUTTOM = Button(pos=(640,400), button_font=get_font(40), base_colour=white, hovering_colour="#d7fcd4", input_text="2 PLAYER", image=None, x_start=475, y_start=350, x_end=780, y_end=450) 
+        QUIT_BUTTON = Button(pos=(640,550), button_font=get_font(40), base_colour=white, hovering_colour="#d7fcd4", input_text="QUIT", image=None, x_start=475, y_start=500, x_end=780, y_end=600) 
+        screen.blit(heading_main_menu_text, menu_rect)
+
+        for button in [ONE_PLAYER_BUTTON, TWO_PLAYER_BUTTOM, QUIT_BUTTON]:
+            button.changecolour(menu_mouse_pos)
+            button.text_update(screen)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if ONE_PLAYER_BUTTON.checkforinput(menu_mouse_pos):
+                    one_player()
+                if TWO_PLAYER_BUTTOM.checkforinput(menu_mouse_pos):
+                    two_player()
+                if QUIT_BUTTON.checkforinput(menu_mouse_pos):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+main_menu()
