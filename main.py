1a8227960c5be6198f011c09863dbecd032bd751
Finian Eagling <Finjude123@outlook.com>
2024-08-14 20:38:50 +0100

    maze generation with main menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from engine import Button
+from random import choice
 pygame.init()
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -12,11 +13,85 @@ black = pygame.Color(0,0,0)
 green = pygame.Color(0,255,0)
 white = pygame.Color(255,255,255)
 run = True
+RES = SCREEN_WIDTH, SCREEN_HEIGHT
+TILE = 20
+collums = 900 // TILE
+rows = 700 // TILE
+clock = pygame.time.Clock()
+distance_from_corner_x = 300
+distance_from_corner_y = 10
 def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
 
 
 def one_player(run):
+    class Cell:
+        def __init__(self, x, y):
+            self.x, self.y = x, y
+            self.walls = {"top": True, "right": True, "bottom": True, "left": True}
+            self.visited = False
+            self.thickness = 2
+    
+        def draw_current_cell(self):
+            x, y = self.x * TILE, self.y * TILE
+            pygame.draw.rect(screen, pygame.Color("yellow"), (x + self.thickness + distance_from_corner_x, y + self.thickness + distance_from_corner_y, TILE - self.thickness, TILE - self.thickness)) 
+
+        def draw(self):
+            x, y = (self.x * TILE) + distance_from_corner_x, (self.y * TILE) + distance_from_corner_y
+            if self.visited:
+                pygame.draw.rect(screen, pygame.Color("black"), (x, y, TILE, TILE))
+            
+            if self.walls["top"]:
+                pygame.draw.line(screen, pygame.Color("red"), (x, y), (x + TILE, y), self.thickness)
+            if self.walls["bottom"]:
+                pygame.draw.line(screen, pygame.Color("red"), (x + TILE, y + TILE), (x, y + TILE), self.thickness)
+            if self.walls["left"]:
+                pygame.draw.line(screen, pygame.Color("red"), (x, y + TILE), (x, y), self.thickness)
+            if self.walls["right"]:
+                pygame.draw.line(screen, pygame.Color("red"), (x + TILE, y), (x + TILE, y + TILE), self.thickness)
+
+        def check_cell(self, x, y):
+            find_index = lambda x, y: x + y * collums
+            if x < 0 or x > collums - 1 or y < 0 or y > rows - 1:
+                return False
+            return grid_cells[find_index(x, y)]
+         
+        def check_neighbours(self):
+            neighbours = []
+            top = self.check_cell(self.x, self.y - 1)
+            bottom = self.check_cell(self.x, self.y + 1)
+            left = self.check_cell(self.x - 1, self.y)
+            right = self.check_cell(self.x + 1, self.y)
+            if top and not top.visited:
+                neighbours.append(top)
+            if bottom and not bottom.visited:
+                neighbours.append(bottom)
+            if left and not left.visited:
+                neighbours.append(left)
+            if right and not right.visited:
+                neighbours.append(right)
+            return choice(neighbours) if neighbours else False
+        
+    def remove_walls(current, next):
+        dx = current.x - next.x
+        if dx == 1:
+            current.walls["left"] = False
+            next.walls["right"] = False
+        elif dx == -1:
+            current.walls["right"] = False
+            next.walls["left"] = False
+        dy = current.y - next.y
+        if dy == 1:
+            current.walls["top"] = False
+            next.walls["bottom"] = False
+        elif dy == -1:
+            current.walls["bottom"] = False
+            next.walls["top"] = False
+
+    grid_cells = [Cell(col, row) for row in range(rows) for col in range(collums)]
+    current_cell = grid_cells[0]
+    stack = []
+ 
     while run:
         screen.fill("#2a0807")
         one_play_mouse_pos = pygame.mouse.get_pos()
@@ -33,10 +108,24 @@ def one_player(run):
                 if ONE_PLAY_BACK.checkforinput(one_play_mouse_pos):
                     main_menu()
     
+        [Cell.draw() for Cell in grid_cells]
+        current_cell.visited = True
+        current_cell.draw_current_cell()
+
+        next_cell = current_cell.check_neighbours()
+        if next_cell:
+            next_cell.visited = True
+            stack.append(current_cell)
+            remove_walls(current_cell, next_cell)
+            current_cell = next_cell
+        elif stack:
+            current_cell = stack.pop()
+        
+        clock.tick(2000)
         pygame.display.update()
         
-
 def two_player(run):
+    
     while run:
         screen.fill("#2a0807")
         two_play_mouse_pos = pygame.mouse.get_pos()
